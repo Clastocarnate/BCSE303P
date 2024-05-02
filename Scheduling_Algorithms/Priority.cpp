00a54410d8bae383eafc01d00842778ab633b710
Clastocarnate <ayush2005au@gmail.com>
2024-05-02 11:15:11 +0530

    Priority Schedulin

---
+++ Scheduling_Algorithms/Priority.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct Process{
+    int PID;
+    int AT;
+    int BT;
+    int P;
+    int CT;
+    int TAT;
+    int WT;
+};
+
+void SJB(Process proc[], int n){
+    int remaining[n];
+    for (int i=0; i<n; i++){
+        remaining[i] = proc[i].BT;
+    }
+
+    int complete = 0;
+    int currentTime = 0;
+    int shortest = 0;
+    int min = 9999;
+
+    while (complete<n){
+        min = 9999;
+        for(int i=0; i<n; i++){
+            if (proc[i].AT<=currentTime && remaining[i]<min && proc[i].P<min){
+                min = proc[i].P;
+                shortest = i;
+            }
+        }
+            remaining[shortest]--;
+           
+            if (remaining[shortest] == 0){
+                complete++;
+                proc[shortest].CT = currentTime + 1;
+                proc[shortest].TAT = proc[shortest].CT - proc[shortest].AT;
+                proc[shortest].WT = proc[shortest].TAT - proc[shortest].BT;
+            }
+             currentTime++;
+        
+    }
+}
+
+void display(Process proc[], int n){
+    cout<<"Shortest Job First"<<endl;
+    cout<<"PID\tAT\tBT\tCT\tTAT\tWT"<<endl;
+    for (int i=0; i<n; i++){
+        cout<<proc[i].PID<<"\t"<<proc[i].AT<<"\t"<<proc[i].BT<<"\t"<<proc[i].CT<<"\t"<<proc[i].WT<<"\t"<<proc[i].TAT<<"\t"<<endl;
+    }
+}
+
+
+int main(){
+    Process proc[4] = {{1,0, 10, 2}, {2, 2, 5, 1}, {3, 3, 2, 0},{4, 5, 20, 3}};
+    SJB(proc, 4);
+    display(proc, 4);
+    return 0;
+
+}
